c4b3354469628dc8cc7a01dd62cb1562c2daf55c
MBMarsell <martbustmar@gmail.com>
2022-07-23 02:34:54 +0200

    add line chart + tweaks on area chart

--- dashboard-1-assets/js/main.js
+++ dashboard-1-assets/js/main.js
@@ -29,6 +29,22 @@ const defaultOptions = {
     },
   },
 
+  markers: {
+    size: 0,
+    colors: undefined,
+    strokeColors: colorCard,
+    strokeWidth: 1,
+    strokeOpacity: 0.5,
+    strokeDashArray: 0,
+    fillOpacity: 1,
+    discrete: [],
+    shape: 'circle',
+    hover: {
+      size: undefined,
+      sizeOffset: 6,
+    },
+  },
+
   tooltip: {
     enabled: true,
     fillSeriesColor: false,
@@ -54,6 +70,26 @@ const defaultOptions = {
         type: 'none',
       },
     },
+  },
+};
+
+// Crosshair default options
+const crosshairsDefault = {
+  crosshairs: {
+    show: true,
+    stroke: {
+      color: 'rgba(255,255,255, 0.25)',
+      width: 1,
+      dashArray: 2,
+    },
+    dropShadow: {
+      enabled: false,
+    },
+    fill: {
+      type: 'solid',
+      color: '#b1b9c4',
+    },
+    position: 'front',
   },
 };
 
@@ -276,13 +312,7 @@ const areaChartOptions = {
 
   colors: [colorDefault, colorPrimary],
   fill: {
-    type: 'gradient',
-    gradient: {
-      shadeIntensity: 0.05,
-      opacityFrom: 0.1,
-      opacityTo: 1,
-      stops: [0, 90, 100],
-    },
+    type: 'fill',
   },
 
   grid: {
@@ -292,6 +322,7 @@ const areaChartOptions = {
   },
   stroke: {
     show: true,
+    width: 2,
   },
   markers: {
     size: 0,
@@ -309,6 +340,7 @@ const areaChartOptions = {
     },
   },
   yaxis: {
+    min: 0,
     max: 100,
     tickAmount: 5,
     labels: {
@@ -331,25 +363,91 @@ const areaChartOptions = {
     axisTicks: {
       show: false,
     },
-    crosshairs: {
-      show: true,
-      stroke: {
-        color: '#rgba(255,255,255, 0.25)',
-        width: 1,
-        dashArray: 2,
-      },
-      dropShadow: {
-        enabled: false,
-      },
-      fill: {
-        type: 'solid',
-        color: '#b1b9c4',
-      },
-      position: 'front',
-    },
+
+    crosshairsDefault,
   },
 };
-
 renderChart(areaChartOptions, 'area-chart');
 
 // Create line Chart
+const lineChartOptions = {
+  ...defaultOptions,
+  series: [
+    {
+      name: 'Series 1',
+      data: [10, 35, 50, 75, 35, 95, 65, 40],
+    },
+    // {
+    //   name: 'Series 2',
+    //   data: [0, 25, 40, 60, 20, 80, 50, 30],
+    // },
+  ],
+  chart: {
+    ...defaultOptions.chart,
+    type: 'area',
+    height: 240,
+  },
+
+  colors: [colorPrimary, colorDefault],
+  fill: {
+    gradient: {
+      type: 'vertical',
+      stops: [0, 100],
+      colorStops: [
+        {
+          offset: 0,
+          color: '#fff',
+          opacity: 0.05,
+        },
+        {
+          offset: 100,
+          color: colorPrimary,
+          opacity: 0,
+        },
+      ],
+    },
+  },
+
+  grid: {
+    strokeDashArray: 3,
+    borderColor: 'rgba(255,255,255,0.05)',
+    padding: { left: 20, right: 20, top: 0, bottom: -8 },
+  },
+  stroke: {
+    show: true,
+    curve: 'smooth',
+    lineCap: 'butt',
+    colors: undefined,
+    width: 2,
+    dashArray: 0,
+  },
+
+  yaxis: {
+    min: 0,
+    max: 100,
+    tickAmount: 5,
+    labels: {
+      style: {
+        fontFamily: 'Sora',
+        colors: colorLabel,
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  xaxis: {
+    labels: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+
+    crosshairsDefault,
+  },
+};
+renderChart(lineChartOptions, 'line-chart');
