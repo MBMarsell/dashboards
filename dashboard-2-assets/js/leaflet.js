3764d6e17f29efc4c38a8642401d074ef5514b7c
MBMarsell <martbustmar@gmail.com>
2022-07-24 22:51:43 +0200

    add markers to map

--- dashboard-2-assets/js/leaflet.js
+++ dashboard-2-assets/js/leaflet.js
@@ -11,3 +11,27 @@ map.keyboard.disable();
 map.dragging.disable();
 
 L.geoJSON(geoJSON).addTo(map);
+
+const markers = [
+  [59.911491, 10.757933], // Oslo
+  [60.397976, 5.324383], // Bergen
+  //   [59.2181, 10.9298], // Fredrikstad
+  [69.649208, 18.955324], // Tromsø
+  [63.446827, 10.421906], // Trondheim
+];
+
+fetch('https://fakestoreapi.com/users?limit=4')
+  .then(res => res.json())
+  .then(json => {
+    console.log(json);
+    json.forEach((user, index) => {
+      let marker = L.marker(markers[index], {
+        icon: L.divIcon({
+          className: 'leaflet-marker-div',
+          html: '',
+        }),
+      }).addTo(map);
+
+      marker.bindTooltip(user.username);
+    });
+  });
